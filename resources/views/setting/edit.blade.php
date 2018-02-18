bcee480405b90752843af0bdf99e18ab2b6ba32c
KID <jyhsu2000@yahoo.com.tw>
2018-02-18 20:16:08 +0800

    Update example

--- resources/views/setting/edit.blade.php
+++ resources/views/setting/edit.blade.php
@@ -8,7 +8,7 @@
         <div class="card-body">
             <h5 class="card-title">網站設定</h5>
             {{ bs()->formGroup(bs()->text('lab_name')->placeholder('顯示於左上角，如：ISLab'))->label('實驗室簡稱')->showAsRow() }}
-            {{ bs()->formGroup(bs()->text('lab_full_name')->placeholder('顯示於首頁、頁尾及標題，如：逢甲大學 安全實驗室'))->label('實驗室全名')->showAsRow() }}
+            {{ bs()->formGroup(bs()->text('lab_full_name')->placeholder('顯示於首頁、頁尾及標題，如：逢甲大學 資訊安全實驗室'))->label('實驗室全名')->showAsRow() }}
         </div>
         <div class="card-body">
             <h5 class="card-title">首頁背景</h5>
