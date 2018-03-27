244693ecc1efb93659b1dbc5322414b945bc4eba
KID <jyhsu2000@yahoo.com.tw>
2018-03-27 13:07:22 +0800

    Update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,13 @@
 <footer class="footer bg-dark">
     <div class="container" style="padding: 15px 10px">
-        <p class="text-muted" style="margin-bottom: 8px">©2018 {{ Setting::get('lab_full_name', config('app.cht_name')) }}. All rights reserved.</p>
+        <ul class="list-inline text-muted">
+            <li class="list-inline-item">
+                ©2018 {{ Setting::get('lab_full_name', config('app.cht_name')) }}.
+                All rights reserved.
+            </li>
+            <li class="list-inline-item">
+                Powered by <a href="https://github.com/jyhsu2000/eLab" target="_blank" class="text-muted">eLab</a>.
+            </li>
+        </ul>
     </div>
 </footer>
