42df079c39ea4ada4b84e29722285d807dc9f5c9
KID <jyhsu2000@yahoo.com.tw>
2019-01-12 15:05:13 +0800

    Add alert message

--- resources/views/user-profile/my/show.blade.php
+++ resources/views/user-profile/my/show.blade.php
@@ -9,5 +9,10 @@
 @endsection
 
 @section('main_content')
+    @if(!$userProfile->is_member)
+        <div class="alert alert-danger">
+            <i class="fas fa-exclamation-triangle mr-2"></i>此資料目前不會顯示於網站上，請告知管理員將您標示為<strong>實驗室成員</strong>
+        </div>
+    @endif
     @include('user-profile.info')
 @endsection
